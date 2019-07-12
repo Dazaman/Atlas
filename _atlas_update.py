c305d24e02ba5d148030b31e87d15610a9bdb644
Danial Azam <dazam92@gmail.com>
2019-07-12 14:35:12 +1000

    Added example folder, known bug with update script

--- _atlas_update.py
+++ _atlas_update.py
@@ -1,6 +1,7 @@
 import os
 import json
 import collections
+import urllib2
 
 hostdir = os.path.abspath(os.path.dirname(__file__))
 atlas_info_filename = os.path.join(hostdir, "_atlas_info.json")
@@ -8,7 +9,9 @@ atlas_url = 'https://basin-genesis-hub.github.io/Atlas/'
 atlas_pages_dirname = 'pages'
 atlas_pages_url = os.path.join(atlas_url, atlas_pages_dirname)
 atlas_index_filename = os.path.join(hostdir, 'index.html')
-
+page_notebook_dir = True 
+header_path = os.path.join(os.path.abspath(os.path.join(os.path.split(os.getcwd())[0], os.pardir)),'site_resources','header.txt')
+target_url = 'https://raw.githubusercontent.com/basin-genesis-hub/Atlas/master/site_resources/header.txt'
 def load_atlas_info():
     try:
         with open(atlas_info_filename, 'r') as file:
@@ -29,15 +32,20 @@ def update_frontpage():
     text.append('layout: default')
     text.append('---')
 
-    # with open('https://github.com/basin-genesis-hub/Atlas/tree/master/site_resources/header.txt','r') as f:
-        # text.append(f.readlines())
+    # with open('https://drive.google.com/file/d/1E9_z3YrBIwThA98yEPCevJFUSJwwj6Yd/view?usp=sharing','r') as f:
+    #     for row in f.readlines():
+    #         text.append(row)
+    header_data = urllib2.urlopen(target_url) # it's a file like object and works just like a file
+    for line in header_data: # files are iterable
+        text.append(line)
+
     text.append('<body>')
     text.append('<p><h1>BGH Atlas</h1></p>')
     
     cat_list = ['Underworld','Badlands','Coupled','PyGplates','Gplates & Citcoms','Uncategorised']
     uw_list = ['Extension','Compression','Strike-slip-tectonic']
     bd_list = ['Basin','Rift','Strike-slip-depositional','Fluvio-deltaic','Case-studies']
-    cp_list = ['Extension','Compression','Strike-slip-coupled']
+    cp_list = ['Coupled','Extension-coupled','Compression-coupled','Strike-slip-coupled']
     pg_list = ['PyGplates']
     gc_list = ['Gplates & Citcoms']
 
@@ -81,8 +89,6 @@ def update_frontpage():
             else:
                 categories['Uncategorised'][cat] = [url] 
 
-    # print 'categories[Badlands][Basin]',type(categories['Badlands']['Basin'])
-
     # categories = collections.OrderedDict(sorted(categories.items()))
     for k,v in categories.items():
         for k1,v1 in categories[k].items():
@@ -91,10 +97,7 @@ def update_frontpage():
     for cat in categories.keys():
         text.append('<p><h2> %s </h2></p>' %(cat.capitalize()))
         text.append('<p><h2>  </h2></p>')
-        
-        # print 'cat', cat
         for sub_cat in categories[cat]:
-            # print 'sub_cat', sub_cat 
             text.append('<p><h3> %s Models </h3>' %(sub_cat.capitalize()))
             text.append('<h3>  </h3>')
             text.append('<table>')
@@ -129,7 +132,6 @@ def update_frontpage():
                 text.append('<tr>')
                 text.append('<td style="text-align: left"><img src="%s" height="75" width="90"></td>'%(image_url))
                 text.append('<td style="text-align: left"><a href="%s"> %s </a></td>'%(atlas_info[url]['url'],atlas_info[url]['name']))
-                # print atlas_info[url]['contributor']
                 text.append('<td style="text-align: left">%s </td>'%(atlas_info[url]['contributor']))
                 
                 text.append('</tr>')
@@ -150,21 +152,21 @@ def update_frontpage():
     with open(atlas_index_filename, 'w') as file:
         file.write('\n'.join(text))
 
+    print "Successfully binded page! "
 def bind_page(page_info,u=None):
-    # print('Page added')
     # Create html:
     command = 'jupyter nbconvert --to html ' + 'page.ipynb'
     os.system(command)
 
     if u == None:
-        # Define page url:
+        page_notebook_dir = True
         page_dirname = os.path.split(os.getcwd())[-1]
         page_url = os.path.join(atlas_pages_url, page_dirname, 'page.html')
-        # print 'page_dirname', page_dirname
-        print 'Running from Individual notebook', page_url
+        print 'Running from Individual notebook'#, page_url
     else:
+        page_notebook_dir = False
         page_url = os.path.join(atlas_pages_url, u, 'page.html')
-        print 'Running from Update notebook ', page_url
+        print 'Running from Update notebook '#, page_url
 
     # Save page info:
     page_dict = {
@@ -188,20 +190,15 @@ def bind_page(page_info,u=None):
     atlas_info = load_atlas_info()
     atlas_info[page_url] = page_dict
     
-    # print 'length before del', len(atlas_info)
     for key, value in atlas_info.items():
-        # print '\nkey  ', key
         directory = os.path.join(*(key.split('/')[-3:-1]))
         file = os.path.join(*(key.split('/')[-3:]))
         local_dir_path = os.path.join(hostdir,directory)
         local_file_path = os.path.join(hostdir,file)
-        # print 'Check dir exists ', os.path.isdir(local_dir_path)
-        # print 'Check file exists' ,os.path.exists(local_file_path)
         if os.path.exists(local_file_path) and os.path.isdir(local_dir_path):
             pass
         else:
             atlas_info.pop(key, None)
-    # print 'length after del',len(atlas_info)
     
     save_atlas_info(atlas_info)
 
